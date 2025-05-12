77307bf6dcd27f60737a09d75f82b85565898ab0
safeer <safeer>
2025-05-12 20:00:05 +0530

    feat(Sonarqube): SQ SAST Support Added
    Internally Using ASPM SAST Job to fetch SQ scanned files, converted to setuptools method due to the use of ASPM SAST Job whl package

--- aspm_cli/utils/validation.py
+++ aspm_cli/utils/validation.py
@@ -4,7 +4,7 @@ from typing import Optional
 from aspm_cli.utils.logger import Logger
 
 
-ALLOWED_SCAN_TYPES = {"iac", "sast"}
+ALLOWED_SCAN_TYPES = {"iac", "sast", "sq-sast"}
 
 class Config(BaseModel):
     SCAN_TYPE: str
@@ -75,6 +75,39 @@ class SASTScannerConfig(BaseModel):
         if not isinstance(v, str) or not v.strip():
             raise ValueError("Unable to retrieve COMMIT_SHA from Git metadata. Please pass the --commit-sha variable")
         return v
+    
+class SQSASTScannerConfig(BaseModel):
+    SONAR_PROJECT_KEY: str
+    SONAR_TOKEN: str
+    SONAR_HOST_URL: str
+    SONAR_ORG_ID: Optional[str] 
+    REPOSITORY_URL: str
+    BRANCH: str
+    COMMIT_SHA: str
+    PIPELINE_URL: Optional[str] 
+
+    @field_validator("REPOSITORY_URL", mode="before")
+    @classmethod
+    def validate_repository_url(cls, v):
+        if not v:
+            raise ValueError("Unable to retrieve REPOSITORY_URL from Git metadata. Please pass the --repo-url variable.")
+        if not isinstance(v, str) or not v.startswith("http"):
+            raise ValueError("Invalid REPOSITORY_URL. It must be a valid URL starting with 'http'.")
+        return v
+
+    @field_validator("BRANCH", mode="before")
+    @classmethod
+    def validate_commit_ref(cls, v):
+        if not isinstance(v, str) or not v.strip():
+            raise ValueError("Unable to retrieve BRANCH from Git metadata. Please pass the --branch variable")
+        return v
+    
+    @field_validator("COMMIT_SHA", mode="before")
+    @classmethod
+    def validate_commit_sha(cls, v):
+        if not isinstance(v, str) or not v.strip():
+            raise ValueError("Unable to retrieve COMMIT_SHA from Git metadata. Please pass the --commit-sha variable")
+        return v
 
 class ConfigValidator:
     def __init__(self, scan_type, accuknox_endpoint, accuknox_tenant, accuknox_label, accuknox_token, softfail):
@@ -116,6 +149,24 @@ class ConfigValidator:
                 COMMIT_SHA=commit_sha,
                 PIPELINE_ID=pipeline_id,
                 JOB_URL=job_url
+            )
+        except ValidationError as e:
+            for error in e.errors():
+                Logger.get_logger().error(f"{error['loc'][0]}: {error['msg']}")
+            exit(1)
+
+
+    def validate_sq_sast_scan(self, sonar_project_key,  sonar_token, sonar_host_url, sonar_org_id, repo_url, branch, commit_sha, pipeline_url):
+        try:
+            self.config = SQSASTScannerConfig(
+                SONAR_PROJECT_KEY=sonar_project_key,
+                SONAR_TOKEN=sonar_token,
+                SONAR_HOST_URL=sonar_host_url,
+                SONAR_ORG_ID = sonar_org_id, 
+                REPOSITORY_URL=repo_url,
+                BRANCH=branch,
+                COMMIT_SHA=commit_sha,
+                PIPELINE_URL=pipeline_url,
             )
         except ValidationError as e:
             for error in e.errors():
