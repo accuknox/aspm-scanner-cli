6244eeb326a32f605c4e41b8d3e7709b81675ca6
Eshrath027 <shaikeshrath027@gmail.com>
2026-04-03 14:50:01 +0530

    testing change

--- aspm_cli/scan/sast.py
+++ aspm_cli/scan/sast.py
@@ -11,7 +11,7 @@ from urllib.parse import urlparse
 import re
 
 class SASTScanner:
-    opengrep_image = os.getenv("SCAN_IMAGE", "public.ecr.aws/k9v9d5v2/accuknox/opengrepjob:0.1.0")
+    opengrep_image = os.getenv("SCAN_IMAGE", "esh279/public.ecr.aws/k9v9d5v2/accuknox/opengrepjob:0.1.0")
     codeassure_image = os.getenv("CODEASSURE_IMAGE", "esh279/codeassure:1.0.0")
     result_file = "results.json"
 
